5c40d9c2d634312e6858413d7c5cf89a3ba2adb8
Alperen Can <alperencan2020@gmail.com>
2025-05-02 07:30:17 +0300

    update

--- ClassLibrary1/Context/DataBaseUserContext.cs
+++ ClassLibrary1/Context/DataBaseUserContext.cs
@@ -19,6 +19,14 @@ namespace ClassLibrary1.Context
             }
         }
         public DbSet<Ability> Abilities { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Hobby> Hobbies { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Certificate> Certificates { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<About> Abouts { get; set; }
+
 
 
 
