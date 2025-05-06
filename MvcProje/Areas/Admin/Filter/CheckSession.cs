d7c3ef00c5e91681d2a2b177660781a90d1d5ae2
Alperen Can <alperencan2020@gmail.com>
2025-05-06 14:29:00 +0300

    Create CheckSession.cs

---
+++ MvcProje/Areas/Admin/Filter/CheckSession.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MvcProje.Areas.Admin.Filter
+{
+    public class CheckSession: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var session = context.HttpContext.Session.GetString("AdminUserName");
+            if (session == null)
+            {
+                context.Result = new RedirectToActionResult("Index", "Login", new { area = "Admin" });
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+    }
+
